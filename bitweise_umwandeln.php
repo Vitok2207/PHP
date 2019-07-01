4f6d3f30bffbdf5472ae51555bdb3a23666b85d7
Vitok2207 <47314375+Vitok2207@users.noreply.github.com>
2019-07-01 19:18:07 +0200

    Create bitweise_umwandeln.php

---
+++ bitweise_umwandeln.php
@@ -0,0 +1,29 @@
+<?php
+	$zahl = "";
+	$binaer = ""; 
+	if ($_GET["Senden"] == "Umwandeln") {
+		$zahl = $_GET["eingabe"];
+		$binaerwerte = array();
+		
+		for ($i = 7; $i >= 0; $i--) {
+			if ($zahl & pow(2, $i)) {
+				$binaerwerte[$i] = 1;
+			} else {
+				$binaerwerte[$i] = 0;
+			}
+		}
+		$binaer = join("", $binaerwerte);
+	}
+?>
+<html>
+	<head>
+		<title>Bin&auml;r</title>
+	</head>
+	<body>
+		<form>
+			<input type="text" name="eingabe" value="<?=$zahl ?>" />
+			<input type="text" name="ausgabe" value="<?=$binaer ?>" />
+			<input type="submit" name="Senden" value="Umwandeln" />
+		</form>
+	</body>
+</html>
